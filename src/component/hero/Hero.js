248a18e740dfd700fad9900963d164f4c9059259
Jeremy Liu <unswmercury@gmail.com>
2023-06-01 01:29:18 +1000

    Add Header component

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -14,11 +14,11 @@ const Hero = ({ movies }) => {
                 <div className="movie-card-container">
                   <div
                     className="movie-card"
-                    style={{ "--img": `url($(movie.backdrops[0]))` }}
+                    style={{ "--img": `url(${movie.backdrops[0]})` }}
                   >
                     <div className="movie-detail">
                       <div className="movie-poster">
-                        <img src={movie.poster} alt="" />
+                        <img src={movie.poster} alt="poster" />
                       </div>
                       <div className="movie-title">
                         <h4>{movie.title}</h4>
