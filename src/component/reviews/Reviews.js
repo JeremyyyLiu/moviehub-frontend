268f2975b4ad20e354fbb28fff2ae847d596a725
Jeremy Liu <unswmercury@gmail.com>
2023-06-01 17:54:33 +1000

    Add reviews component

---
+++ src/component/reviews/Reviews.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+
+import React from "react";
+
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+  const revText = useRef();
+  let params = useParams();
+  const movieId = params.movieId;
+
+  useEffect(() => {
+    getMovieData(movieId);
+  }, []);
+
+  return <div></div>;
+};
+
+export default Reviews;
